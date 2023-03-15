97aa6bc55631a0a806c3880c89e810463dcc60cb
Aixa Filsinger <macafil99@gmail.com>
2023-03-14 23:10:20 -0300

    Se termino el ejercicio2-func

--- js/ejercicio2_func.js
+++ js/ejercicio2_func.js
@@ -8,6 +8,15 @@
 let cadena = prompt("Ingrese una frase");
 
 function infoCadena (cadena) {
-    if(cadena == cadena.toUpperCase())
+    if(cadena == cadena.toUpperCase()){
+        document.write("La cadena esta formada por Mayúsculas :)")
+    }else 
+    if(cadena == cadena.toLowerCase()){
+        document.write("La cadena esta formada por Minúsculas :)")
+    } else{
+        document.write("La cadena esta formada por Minúsculas y Mayúsculas :)")
+    }
 
 }
+
+infoCadena(cadena);
